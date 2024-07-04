28362a0c5076be58bbd19f8505a96939091eb21a
Amit Ghatage <amitghatage001@gmail.com>
2024-07-04 13:14:41 +0530

    created get mapping for array, boolean and arraylist and returned them on server

--- myfirstproject/src/main/java/com/springBoot/myfirstproject/Myclass.java
+++ myfirstproject/src/main/java/com/springBoot/myfirstproject/Myclass.java
@@ -3,11 +3,36 @@ package com.springBoot.myfirstproject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class Myclass {
 
     @GetMapping("/name")
     public String getname(){
+
         return "Amit Ghatage";
+    }
+
+    @GetMapping("/array")
+    public int[] getArray(){
+        int[] arr = {1,23,45,565};
+        return arr;
+    }
+
+    @GetMapping("/boolean")
+    public boolean getBoolean(){
+        return true;
+    }
+
+    @GetMapping("/list")
+    public ArrayList<String> getList(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Amit Ghatage");
+        list.add("Anil Ghatage");
+        list.add("kalpana Ghatage");
+        list.add("Anuja Ghatage");
+
+        return list;
     }
 }
